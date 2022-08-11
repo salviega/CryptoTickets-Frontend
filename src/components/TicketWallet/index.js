27ad0174385ef906a060f9cd7fac0dda16f5294f
Santiago Viana Vera <salviega6@gmail.com>
2022-08-11 14:31:54 -0500

    [REFACTOR] TickectWallet

--- src/components/TicketWallet/index.js
+++ src/components/TicketWallet/index.js
@@ -6,10 +6,9 @@ import "./TicketWallet.scss";
 import { Button } from "@chakra-ui/react";
 
 function TicketWallet({walletDesconected, setWalletDesconected}) {
-
   const state = useSelector(state => state);
   const dispatch = useDispatch();
-
+  
   const createProvider = (content) => {
     return {
       type: "walletConected",
@@ -21,37 +20,20 @@ function TicketWallet({walletDesconected, setWalletDesconected}) {
     };
   };
 
-  // const requestAccount = async (provider) => {
-  //   if (window.ethereum) {
-  //     try {
-  //       const accounts = await provider.send("eth_requestAccounts", []);
-  //       setWallet(accounts[0]);
-  //     } catch (error) {
-  //       console.log(error);
-  //       console.log("Error connecting...");
-  //     }
-  //   } else {
-  //     alert("Meta Mask not detected");
-  //   }
-  // };
-  //
-
   const connectWallet = async () => {
     if (typeof window.ethereum !== "undefined") {
-      
-      const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
-      const accounts = await web3Provider.send("eth_requestAccounts", []);
-      const wallet = accounts[0];
-      const web3Signer = web3Provider.getSigner();
-      
-      //await requestAccount(web3Provider);
-      // let buttonMessage = "connect wallet";
-      // if (walletConnected) buttonMessage = "..." + String(wallet).slice(38);
-
-      dispatch(createProvider({web3Provider, web3Signer, wallet}));
-      setWalletDesconected(false);
+      try {
+        const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await web3Provider.send("eth_requestAccounts", []);
+        const wallet = accounts[0];
+        const web3Signer = web3Provider.getSigner();
+        dispatch(createProvider({web3Provider, web3Signer, wallet}));
+        setWalletDesconected(false);
+      } catch(error) {
+        console.log(error)
+      }
     } else {
-      // desconect wallet
+      alert("Meta Mask not detected");
     }
   };
 
